fea729893c869e797f8b3dd0228b39ea0a8ab0ae
Peter Hellberg <peter@c7.se>
2014-10-20 23:22:47 +0200

    Changed req -> r, comments, getenv, PORT variable

--- tdtool-api.go
+++ tdtool-api.go
@@ -1,48 +1,55 @@
 package main
 
 import (
-	"github.com/bmizerany/pat"
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
+
+	"github.com/bmizerany/pat"
 )
 
 func main() {
 	m := pat.New()
 
 	m.Get("/", http.HandlerFunc(
-		func(w http.ResponseWriter, req *http.Request) {
+		func(w http.ResponseWriter, r *http.Request) {
 			Output(w, exec.Command("tdtool", "-l"))
 		}))
 
 	m.Put("/:device/on/sync", http.HandlerFunc(
-		func(w http.ResponseWriter, req *http.Request) {
-			Output(w, DeviceCommand("--on", req))
+		func(w http.ResponseWriter, r *http.Request) {
+			Output(w, DeviceCommand("--on", r))
 		}))
 
 	m.Put("/:device/on", http.HandlerFunc(
-		func(w http.ResponseWriter, req *http.Request) {
-			Async(w, DeviceCommand("--on", req))
+		func(w http.ResponseWriter, r *http.Request) {
+			Async(w, DeviceCommand("--on", r))
 		}))
 
 	m.Put("/:device/off/sync", http.HandlerFunc(
-		func(w http.ResponseWriter, req *http.Request) {
-			Output(w, DeviceCommand("--off", req))
+		func(w http.ResponseWriter, r *http.Request) {
+			Output(w, DeviceCommand("--off", r))
 		}))
 
 	m.Put("/:device/off", http.HandlerFunc(
-		func(w http.ResponseWriter, req *http.Request) {
-			Async(w, DeviceCommand("--off", req))
+		func(w http.ResponseWriter, r *http.Request) {
+			Async(w, DeviceCommand("--off", r))
 		}))
 
 	http.Handle("/", m)
-	err := http.ListenAndServe(":8080", nil)
+
+	port := getenv("PORT", "8080")
+
+	log.Printf("Listening on http://0.0.0.0:%s", port)
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+// Output writes the output of a command to the provided response writer
 func Output(w http.ResponseWriter, cmd *exec.Cmd) {
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
@@ -56,6 +63,7 @@ func Output(w http.ResponseWriter, cmd *exec.Cmd) {
 	cmd.Wait()
 }
 
+// Async executes a command asynchronously
 func Async(w http.ResponseWriter, cmd *exec.Cmd) {
 	err := cmd.Start()
 	if err != nil {
@@ -65,7 +73,15 @@ func Async(w http.ResponseWriter, cmd *exec.Cmd) {
 	w.WriteHeader(202)
 }
 
-func DeviceCommand(option string, req *http.Request) *exec.Cmd {
-	device := req.URL.Query().Get(":device")
-	return exec.Command("tdtool", option, device)
+// DeviceCommand returns a tdtool command for :device and option
+func DeviceCommand(option string, r *http.Request) *exec.Cmd {
+	return exec.Command("tdtool", option, r.URL.Query().Get(":device"))
+}
+
+func getenv(key, fallback string) string {
+	v := os.Getenv(key)
+	if v != "" {
+		return v
+	}
+	return fallback
 }
